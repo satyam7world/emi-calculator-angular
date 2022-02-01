3094f552f528722efab0dc5c1cbddc6b8ba4dc6d
rider <rdxsttymm@gmail.com>
2022-02-01 07:19:31 +0530

    still wrong but gonna change. came after learning

--- src/app/otros/utils/Calculator.ts
+++ src/app/otros/utils/Calculator.ts
@@ -1,4 +1,5 @@
 import EmiRawData from "../datapojo/EmiRawData";
+import emiRawData from "../datapojo/EmiRawData";
 
 class Calculator {
   calculateEmi(data: EmiRawData): string {
@@ -7,9 +8,14 @@ class Calculator {
     let r = data.interest
     let n = t * 12 // tenure in months
 
+    console.log(data, n)
+
     let firstPart = (p * r)
+    console.log("firstPart :", firstPart)
     let secondUpPart = ((1 + r) ** n)
+    console.log("secondPart", secondUpPart)
     let thirdDownPart = (((1 + r) ** n) - 1)
+    console.log("thirdDownPart", thirdDownPart)
 
     let calculationMergeThirdPart = firstPart * (secondUpPart / thirdDownPart)
 
